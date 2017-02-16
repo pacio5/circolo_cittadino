30c51f9a9fe8dd631bc404929bd435b5d4494a1e
Elia Pacioni <eliapacioni@gmail.com>
2017-02-16 15:50:00 +0100

    MySql
    Creazione della classe che gestisce i parametri del database e la
    connessione con lo stesso

---
+++ src/main/Circolo.java
@@ -0,0 +1,13 @@
+package main;
+
+/**
+ * @author eliapacioni
+ *
+ */
+public class Circolo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/utility/MySql.java
@@ -0,0 +1,105 @@
+/**
+ * 
+ */
+package utility;
+
+import java.sql.*;
+
+/* Manipolazione file XML */
+
+import org.w3c.dom.*;
+import org.xml.sax.*;
+import javax.xml.parsers.*;
+import javax.xml.xpath.*;
+import java.io.*;
+
+/**
+ * @author eliapacioni
+ *
+ */
+public class MySql {
+
+	// Utilizzare la lettura da file per caricare le impostazioni del database,
+	// considerare che è una classe statica
+	private String driver;
+	private String url;
+	private String utente;
+	private String password;
+	private Connection conn = null;
+
+	public MySql() {
+		try {
+			File inputFile = new File("resources/config.xml");
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+
+			Document doc = builder.parse(inputFile);
+			doc.getDocumentElement().normalize();
+
+			XPath xPath = XPathFactory.newInstance().newXPath();
+
+			String expression = "/database";
+
+			NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(doc, XPathConstants.NODESET);
+
+			Node nNode = nodeList.item(0);
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element eElement = (Element) nNode;
+
+				driver = eElement.getElementsByTagName("driver").item(0).getTextContent();
+				url = eElement.getElementsByTagName("url").item(0).getTextContent();
+				utente = eElement.getElementsByTagName("user").item(0).getTextContent();
+				password = eElement.getElementsByTagName("password").item(0).getTextContent();
+			}
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (XPathExpressionException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// Effettua la connessione al db
+	public Connection connessione() {
+		try {
+			// Carico il driver
+			Class.forName(driver);
+		} catch (ClassNotFoundException e1) {
+			// Il driver non può essere caricato
+			System.out.println("Driver non trovato");
+			System.exit(1);
+		}
+		try {
+			// Apro la connessione
+			conn = DriverManager.getConnection(url);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+	// Chiude la connessione al db
+	public void chiudiConnessione() {
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getConn() {
+		return conn;
+	}
+
+	public String toString() {
+		return "Il driver usato per la connessione al database è: " + driver + " l'url del db è: " + url
+				+ " e il suo utente è: " + utente + " la sua password è: " + password;
+	}
+
+}
