bdae522d2f1ce5624fa9b130d28c94817816139a
Riccardo <smerilliriccardo@gmail.com>
2017-02-16 19:02:02 +0100

    Formattazione codice
    Formattazione codice per classe Quota e Versamento

--- src/entita/Quota.java
+++ src/entita/Quota.java
@@ -9,66 +9,68 @@ public class Quota {
 	private String tipologia;
 	private Date dataInizio;
 	private Date dataFine;
-	
-	public Quota(){
+
+	public Quota() {
 		this(-1, 0, null, null, null);
 	}
-	
-	public Quota(int cod, double value, String type, Date dataI, Date dataF){
+
+	public Quota(int cod, double value, String type, Date dataI, Date dataF) {
 		id = cod;
 		valore = value;
 		tipologia = type;
 		dataInizio = dataI;
 		dataFine = dataF;
 	}
-	
-	public String toString(){
-		return "Quota[Id = "+ id +", Valore = "+ valore +", Tipologia = "+ tipologia +", Data Inizio = "+ dataInizio +", Data Fine = "+ dataFine +"]";
-	}	
-	
-	public boolean equals(Quota x){
-		return (id == x.id && valore == x.valore && tipologia.equals(x.tipologia) && !dataInizio.after(x.dataInizio) && !dataInizio.before(x.dataInizio) && !dataFine.after(x.dataFine) && !dataFine.before(x.dataFine));
+
+	public String toString() {
+		return "Quota[Id = " + id + ", Valore = " + valore + ", Tipologia = " + tipologia + ", Data Inizio = "
+				+ dataInizio + ", Data Fine = " + dataFine + "]";
 	}
-	
+
+	public boolean equals(Quota x) {
+		return (id == x.id && valore == x.valore && tipologia.equals(x.tipologia) && !dataInizio.after(x.dataInizio)
+				&& !dataInizio.before(x.dataInizio) && !dataFine.after(x.dataFine) && !dataFine.before(x.dataFine));
+	}
+
 	/* Lettura attributi */
-	public int getId(){
+	public int getId() {
 		return id;
 	}
-	
-	public double getValore(){
+
+	public double getValore() {
 		return valore;
 	}
-	
-	public String getTipologia(){
+
+	public String getTipologia() {
 		return tipologia;
 	}
-	
-	public Date getDataI(){
+
+	public Date getDataI() {
 		return dataInizio;
 	}
-	
-	public Date getDataF(){
+
+	public Date getDataF() {
 		return dataFine;
 	}
-	
+
 	/* Inserimento attributi */
-	public void setId(int cod){
+	public void setId(int cod) {
 		id = cod;
 	}
-	
-	public void setValore(double value){
+
+	public void setValore(double value) {
 		valore = value;
 	}
-	
-	public void setTipologia(String type){
+
+	public void setTipologia(String type) {
 		tipologia = type;
 	}
-	
-	public void setDataI(Date dataI){
+
+	public void setDataI(Date dataI) {
 		dataInizio = dataI;
 	}
-	
-	public void setDataF(Date dataF){
+
+	public void setDataF(Date dataF) {
 		dataFine = dataF;
 	}
 }
--- src/entita/Versamento.java
+++ src/entita/Versamento.java
@@ -10,67 +10,68 @@ public class Versamento {
 	private String[] mesi;
 	private Date data;
 	private String descrizione = new String();
-	
-	public Versamento(){
+
+	public Versamento() {
 		this(-1, null, 0, null, null);
 	}
-	
-	public Versamento(int cod, String[] month, double value, Date dataV, String desc){
+
+	public Versamento(int cod, String[] month, double value, Date dataV, String desc) {
 		id = cod;
 		mesi = month;
 		importo = value;
 		data = dataV;
 		descrizione = desc;
 	}
-	
-	public boolean equals(Versamento x){
-		return (id == x.id && mesi.equals(x.mesi) && importo == x.importo && !data.after(x.data) && !data.before(x.data) && descrizione.equals(x.descrizione));
+
+	public boolean equals(Versamento x) {
+		return (id == x.id && mesi.equals(x.mesi) && importo == x.importo && !data.after(x.data) && !data.before(x.data)
+				&& descrizione.equals(x.descrizione));
 	}
-	
+
 	public String toString() {
 		return "Versamento [Id=" + id + ", Importo=" + importo + ", Mesi=" + Arrays.toString(mesi) + ", Data=" + data
 				+ ", Descrizione=" + descrizione + "]";
 	}
-	
+
 	/* Lettura attributi */
-	public int getId(){
+	public int getId() {
 		return id;
 	}
-	
-	public String[] getMesi(){
+
+	public String[] getMesi() {
 		return mesi;
 	}
-	
-	public double getImporto(){
+
+	public double getImporto() {
 		return importo;
 	}
-	
-	public Date getData(){
+
+	public Date getData() {
 		return data;
 	}
-	
-	public String getDescrizione(){
+
+	public String getDescrizione() {
 		return descrizione;
 	}
-	
+
 	/* Inserimento attributi */
-	public void setId(int cod){
+	public void setId(int cod) {
 		id = cod;
 	}
-	
-	public void setMesi(String[] month){
+
+	public void setMesi(String[] month) {
 		mesi = month;
 	}
-	
-	public void setImporto(double value){
+
+	public void setImporto(double value) {
 		importo = value;
 	}
-	
-	public void setData(Date dataV){
+
+	public void setData(Date dataV) {
 		data = dataV;
 	}
-	
-	public void setDescrizione(String desc){
+
+	public void setDescrizione(String desc) {
 		descrizione = desc;
 	}
 }
