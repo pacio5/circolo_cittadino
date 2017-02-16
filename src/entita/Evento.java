c4ac68e252b070c931c6337a959fe7e058853b95
Elia Pacioni <eliapacioni@gmail.com>
2017-02-16 16:59:15 +0100

    Classe evento
    Creazione classe evento con costruttore, get, set e toString

---
+++ src/entita/Evento.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package entita;
+
+import java.sql.Date;
+/**
+ * @author eliapacioni
+ *
+ */
+public class Evento {
+	private String id;
+	private String nome;
+	private Date data;
+	private String descrizione;
+	private int posti;
+	private String luogo;
+	private double prezzo;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String name) {
+		nome = name;
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date dataE) {
+		data = dataE;
+	}
+	public String getDescrizione() {
+		return descrizione;
+	}
+	public void setDescrizione(String desc) {
+		descrizione = desc;
+	}
+	public int getPosti() {
+		return posti;
+	}
+	public void setPosti(int numero) {
+		this.posti = numero;
+	}
+	public String getLuogo() {
+		return luogo;
+	}
+	public void setLuogo(String posto) {
+		luogo = posto;
+	}
+	public double getPrezzo() {
+		return prezzo;
+	}
+	public void setPrezzo(double costo) {
+		prezzo = costo;
+	}
+	public String getId() {
+		return id;
+	}
+	
+	@Override
+	public String toString() {
+		return "Evento [id=" + id + ", nome=" + nome + ", data=" + data + ", descrizione=" + descrizione + ", posti="
+				+ posti + ", luogo=" + luogo + ", prezzo=" + prezzo + "]";
+	}
+	
+	
+	
+}
