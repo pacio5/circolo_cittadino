7f68f6185e75f7862c9877cbd06fbc3b50220b2e
Elia Pacioni <eliapacioni@gmail.com>
2017-02-16 16:52:46 +0100

    Classe Sala
    Creazione classe sala con costruttore, get, set e toString

---
+++ src/entita/Sala.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package entita;
+
+/**
+ * @author eliapacioni
+ *
+ */
+public class Sala {
+	private String id;
+	private String nome;
+	private int capienza;
+	private String descrizione;
+	private double tariffa;
+	
+	public Sala(String codice, String name, int posti, String desc, double prezzo){
+		id = codice;
+		nome = name;
+		capienza = posti;
+		descrizione = desc;
+		tariffa = prezzo;
+	}
+	
+	public Sala(){
+		this(null,null,-1, null, 0);
+	}
+	
+	public String getId(){
+		return id;
+	}
+	
+	public String getNome(){
+		return nome;
+	}
+	
+	public int getCapienza(){
+		return capienza;
+	}
+	
+	public String getDescrizione(){
+		return descrizione;
+	}
+	
+	public double getTariffa(){
+		return tariffa;
+	}
+	
+	public void setNome(String name){
+		nome = name;
+	}
+	
+	public void setCapienza(int posti){
+		capienza = posti;
+	}
+	
+	public void setDescrizione(String desc){
+		descrizione = desc;
+	}
+	
+	public String toString(){
+		return "La sala: "+ id+ " " + nome + " ha una capienza di: " + capienza + ", la sua tariffa è: " + tariffa + ". Breve descrizione della sala: "+ descrizione;
+	}
+}
