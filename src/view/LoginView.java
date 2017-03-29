6c5d2e520740576407bf34c5faa6efec77209161
Elia Pacioni <eliapacioni@gmail.com>
2017-03-29 15:53:47 +0200

    Layout login
    Impostato absolute layout e bloccato il ridimensionamento delle
    finestra.

--- src/view/LoginView.java
+++ src/view/LoginView.java
@@ -1,7 +1,6 @@
 package view;
 
 import javax.swing.JFrame;
-import java.awt.FlowLayout;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
@@ -26,21 +25,26 @@ public class LoginView {
 	 */
 	private void initialize() {
 		frmCircoloCittadino = new JFrame("Circolo Cittadino - Login");
+		frmCircoloCittadino.setResizable(false);
 		frmCircoloCittadino.setBounds(100, 100, 450, 300);
 		frmCircoloCittadino.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JLabel lblUsername = new JLabel("Username: ");
+		lblUsername.setBounds(23, 10, 70, 16);
 
 		textFieldUsername = new JTextField();
+		textFieldUsername.setBounds(98, 5, 130, 26);
 		textFieldUsername.setColumns(10);
 
 		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setBounds(233, 10, 59, 16);
 
 		Password = new JPasswordField();
+		Password.setBounds(297, 5, 130, 26);
 		Password.setColumns(10);
 
 		btnAccedi = new JButton("Accedi");
-		FlowLayout flowLayout = new FlowLayout(FlowLayout.CENTER, 5, 5);
-		frmCircoloCittadino.getContentPane().setLayout(flowLayout);
+		btnAccedi.setBounds(182, 36, 86, 29);
+		frmCircoloCittadino.getContentPane().setLayout(null);
 		frmCircoloCittadino.getContentPane().add(lblUsername);
 		frmCircoloCittadino.getContentPane().add(textFieldUsername);
 		frmCircoloCittadino.getContentPane().add(lblPassword);
